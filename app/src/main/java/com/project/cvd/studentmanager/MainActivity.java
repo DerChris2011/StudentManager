532e7a08c8cbe1637ee0eb7c018dd6824c0f1d48
Christoph Fandrich <chrisfand@googlemail.com>
2018-07-04 10:01:09 +0200

    Initial commit

---
+++ app/src/main/java/com/project/cvd/studentmanager/MainActivity.java
@@ -0,0 +1,21 @@
+package com.project.cvd.studentmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_Test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
